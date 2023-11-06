934ca92b5a5c5db225643e2de38b943a671dfffb
Maicon <maicon.carlone@gmail.com>
2023-11-06 14:07:18 -0300

    refactor: rev 01

--- index.js
+++ index.js
@@ -1,35 +1,65 @@
 
 // Tudo Carregado em index.html
 window.addEventListener("load", () => {
+
+    // Fim do estado de Preload
     console.log('All content Loaded');
 
-    // Função para carregar e manipular a imagem
-    function carregarImagem(lockUp) {
+    // Determinando tamanho e propriedades do cavas funcional e do seu div
+    function defineCanvas(canvasUtil) {
         const imagem = document.getElementById("imgOne");
-        const canvas = document.getElementById("canvas");
-        const ctx = canvas.getContext("2d");
+        const canvas = document.getElementById(canvasUtil);
 
         // Adicionei um ID aos divs de coluna
         const canvasContainer = document.getElementById("divTwo");
         canvasContainer.style.backgroundColor = "white";
 
-        // Obtém as dimensões do contêiner do canvas
+        // Dimensões do contêiner do canvas
         const containerWidth = imagem.width;
         const containerHeight = imagem.height;
 
-        // Define o tamanho do canvas para corresponder ao tamanho do contêiner
+        // Definindo o tamanho do canvas para bater com o tamanho do contêiner
         canvas.width = containerWidth;
         canvas.height = containerHeight;
+    }
+
+    // Mostra o Div Principal depois do Loading
+    function mostrarDiv() {
+        const div = document.getElementById("mainDiv");
+        div.style.display = "flex";
+    }
+
+    // Remover o elemento de "loading"
+    function removerLoading() {
+        const loadingDiv = document.getElementById("loadingDiv");
+        if (loadingDiv) {
+            document.body.removeChild(loadingDiv);
+        }
+    }
+
+    // Aplicando Propriedades ao Canvas
+    defineCanvas("canvas");
+    defineCanvas("canvasOculto");
+
+    // Defina um vetor para armazenar os dados da imagem
+    const imageArray = [];
+
+    // Função para carregar e manipular a imagem e salvar os dados no vetor
+    function carregarImagem(lockUp) {
+        const imagem = document.getElementById("imgOne");
+        const canvas = document.getElementById("canvasOculto");
+        const ctx = canvas.getContext("2d");
 
         // Redimensiona a imagem para se ajustar ao tamanho do canvas
         ctx.drawImage(imagem, 0, 0, canvas.width, canvas.height);
 
-        // Obtém os dados dos pixels da imagem
+        // Pegando os dados dos pixels da imagem no canvas
         let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
         // Percorre todos os pixels e manipula as cores
         for (let i = 0; i < imageData.data.length; i += 4) {
-            // Manipule as cores dos pixels aqui (inverter as cores)
+
+            // Manipulei as cores dos pixels aqui (inverti as cores)
             imageData.data[i] = 255 - imageData.data[i]; // Vermelho
             imageData.data[i + 1] = 255 - imageData.data[i + 1]; // Verde
             imageData.data[i + 2] = 255 - imageData.data[i + 2]; // Azul
@@ -54,38 +84,56 @@ window.addEventListener("load", () => {
             }
         }
 
-        // Atualiza a imagem no canvas com as cores manipuladas
-        ctx.putImageData(imageData, 0, 0);
-    }
+        // Adicionando os dados da imagem manipulada ao novo vetor
+        imageArray.push(imageData);
 
+        // Não atualizar a imagem aqui
+    };
 
-    // Chama a função quando a imagem estiver carregada
-    carregarImagem(0);
+    // Plotando as imagens do vetor
+    function plotImagesFromArray(n) {
+        const canvas = document.getElementById("canvas");
+        const ctx = canvas.getContext("2d");
+        ctx.putImageData(imageArray[n], 0, 0);
+    };
 
+    // Ctes de Animação
     const fps = 20;
     const timeInterval = 1000 / fps;
     let timer = 0;
     let lastTime = 0;
     const initialValue = 230;
-    let currentCtrl = initialValue;
-    
+    const finalValue   = 170;
+    let currentCtrl = 0;
+
+    // Loop de Geração de Img
+    for (let i = initialValue; i >= finalValue; i--) {
+        carregarImagem(i);
+    };
+
+    // Função de Animação
     function animate(timeStamp) {
         const delta = timeStamp - lastTime;
         lastTime = timeStamp;
 
         if (timer > timeInterval) {
-            //console.log(timer);
-            currentCtrl -= 1;
-            carregarImagem(currentCtrl);
-            if (currentCtrl < 170) currentCtrl = initialValue;
+
+            //console.log(currentCtrl)
+            plotImagesFromArray(currentCtrl);
+
+            // Condições de Loop Continuo
+            currentCtrl++;
+            if (currentCtrl >= imageArray.length) currentCtrl = 0;
             timer = 0;
         } else {
             timer += delta;
-        }
+        };
 
         requestAnimationFrame(animate);
-    }
+    };
 
+    removerLoading();
     animate(0);
+
 
 });
